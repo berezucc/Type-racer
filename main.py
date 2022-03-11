3cbcd3a1a6f06c818f17eb03aff92e30cc36210d
Nikita Berezyuk ☭ <96497560+berezucc@users.noreply.github.com>
2022-03-10 23:57:36 -0500

    Final changes

--- main.py
+++ main.py
@@ -26,10 +26,8 @@ def screenShot():
 """Image Function --> Reads Text From Image"""
 def imageReader():
     screenShot()
-    #img = cv2.imread('text.JPG') #Reads image file
     img = cv2.imread('screen.png') #Reads image file
-    text = pytesseract.image_to_string(img) #Extracts text from imageFile
-
+    text = str(pytesseract.image_to_string(img, lang ="eng")) #Extracts text from imageFile
     return text #Returns text
 
 """Writer Function --> Types Keys"""
@@ -39,23 +37,33 @@ def writer():
     # Cleans text and fixes any bugs
     text = text.split("change display format")
     text = text[0].replace("change display format","")
+    text = ' '.join(text.splitlines())
     if(text[0] == "["):
         text = text[1:]
-    if("|" in text or "1" in text):
+    if("|" in text or "1" in text or "\\" in text or "!" in text):
         text = text.replace("|","I")
         text = text.replace("1","I")
+        text = text.replace("\\","I")
+        text = text.replace("!","I")
 
+    print(text)
     # 3 Second delay, begins typing
-    time.sleep(4)
-    #keyboard.write(text)
+    time.sleep(3)
     for char in text:
-        keyboard.press(char)
-        keyboard.release(char)
-        time.sleep(0.05)
+        keyboard.write(char)
+        #print(char)
+        time.sleep(0.01)
+    
 
 """Main Function"""
 if __name__ == "__main__":
     writer() #Calls writer function
 """
+The time has come for man to plant the seed of his highest hope. His soil is still rich
+enough. But one day this soil will be poor and domesticated, and no tall tree will be
+able to grow in it.
+
+
 
 """
+
